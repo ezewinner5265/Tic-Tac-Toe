d697bbccc5bfdca8c52d88c4eb54f409f09ea040
Winezky Umpire <ezewinner5265@gmail.com>
2025-12-23 15:07:08 +0100

    Initial commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,8 @@
+const Square = ({ value, onSquareClick, isHighlighted }) => {
+
+    return (
+        <button className={`square ${isHighlighted ? 'highlight' : ''}`} onClick={onSquareClick}>{value}</button>
+    )
+}
+
+export default Square;
